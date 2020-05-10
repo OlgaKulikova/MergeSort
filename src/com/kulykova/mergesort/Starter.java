abf0a12f5c45ead4b23047c433d6491c0d1342fd
Olga Kulikova <olga.kulikova@paazl.com>
2020-05-10 17:30:19 +0300

    test commit

--- src/com/kulykova/mergesort/Starter.java
+++ src/com/kulykova/mergesort/Starter.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Starter {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[10];
@@ -22,6 +23,5 @@ public class Starter {
 
         System.out.println("And now you have ordered array of numbers");
         System.out.println(Arrays.toString(numbers));
-
     }
 }
